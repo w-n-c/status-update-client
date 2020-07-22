cea846de9e3cf4b630442e352a47b699af303a7d
Will Newell <newellwm@gmail.com>
2020-07-22 17:06:41 -0400

    Add event creation form

---
+++ src/routes/events/new.svelte
@@ -0,0 +1,40 @@
+<script>
+	import apiBuilder from '../../api'
+	let Type
+	let Title
+	let Description
+
+	async function createEvent() {
+		try {
+			const api = await apiBuilder()
+			const result = await api.events.post({ Type, Title, Description })
+			alert('Success!')
+		} catch (error) {
+			console.error(error)
+		}
+	}
+</script>
+
+<form on:submit|preventDefault={createEvent}>
+	<fieldset>
+		<legend>Create An Event:</legend>
+		<label>
+			Type:
+			<select bind:value={Type}>
+				<option>Training</option>
+				<option>Application</option>
+				<option>Other</option>
+			</select>
+		</label>
+		<label>
+			Title:
+			<input type="text" required bind:value={Title} />
+		</label>
+		<br />
+		<label>
+			Description:
+			<textarea required bind:value={Description} />
+		</label>
+	</fieldset>
+	<input type="submit" value="create event" />
+</form>
