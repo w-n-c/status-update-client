1984f28b234eff211b1499e3a5b3db146a65f7f3
Will Newell <newellwm@gmail.com>
2020-07-22 14:55:24 -0400

    Add Amplify initialization

--- src/client.js
+++ src/client.js
@@ -1,4 +1,13 @@
 import * as sapper from '@sapper/app';
+import Amplify from 'aws-amplify'
+
+Amplify.configure({
+	Auth: {
+		region: 'us-east-2',
+		userPoolId: 'us-east-2_1EqvjhRsp',
+		userPoolWebClientId: '2njqa3sijuq8poccgokumm948m'
+	}
+})
 
 sapper.start({
 	target: document.querySelector('#sapper')
