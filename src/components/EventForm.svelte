4a6bd8dee1d0259dcad92209530e8e760930a42a
Will Newell <newellwm@gmail.com>
2020-07-22 23:42:17 -0400

    Add form components

---
+++ src/components/EventForm.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let handleSubmit
+
+	let Type
+	let Title
+	let Description
+
+	const onSubmit = () => {
+		handleSubmit({ Type, Title, Description })
+	}
+</script>
+
+<form on:submit|preventDefault={onSubmit}>
+	<label>
+		Type:
+		<select bind:value={Type}>
+			<option>Training</option>
+			<option>Application</option>
+			<option>Other</option>
+		</select>
+	</label>
+	<label>
+		Title:
+		<input type="text" required bind:value={Title} />
+	</label>
+	<br />
+	<label>
+		Description:
+		<textarea required bind:value={Description} />
+	</label>
+	<input type="submit" value="add event" />
+</form>
---
+++ src/components/StatusForm.svelte
@@ -0,0 +1,39 @@
+<script>
+	let Type, Title, Progression, Note
+	export let handleSubmit
+	const onSubmit = () => {
+		handleSubmit({
+			Note,
+			Progression,
+			PostedOn: Date.now(),
+			EventId: { Type, Title },
+		})
+	}
+</script>
+
+<form on:submit|preventDefault={onSubmit}>
+	<fieldset>
+		<legend>What did you work on?</legend>
+		<label>
+			Type:
+			<select bind:value={Type}>
+				<option>Training</option>
+				<option>Application</option>
+				<option>Other</option>
+			</select>
+		</label>
+		<label>
+			Title:
+			<input type="text" required bind:value={Title} />
+		</label>
+	</fieldset>
+	<label>
+		Progress:
+		<textarea required bind:value={Progression} />
+	</label>
+	<label>
+		Note:
+		<textarea required bind:value={Note} />
+	</label>
+	<input type="submit" value="add event" />
+</form>
