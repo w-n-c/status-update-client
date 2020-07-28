36168fd434f29b73194bcf27153faabe3602ec79
Will Newell <newellwm@gmail.com>
2020-07-28 15:19:16 -0400

    Add nav header with logout button

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,3 +1,22 @@
+<script>
+	import { Auth } from 'aws-amplify'
+	import { goto } from '@sapper/app'
+	import apiBuilder from '../api'
+	export let segment
+	const fetchUser = async () => {
+		const api = await apiBuilder()
+		return api.currentUser
+	}
+	async function logout() {
+		try {
+			await Auth.signOut()
+			goto('/')
+		} catch (error) {
+			console.log('error signing out: ', error)
+		}
+	}
+</script>
+
 <style>
 	main {
 		position: relative;
@@ -9,6 +28,31 @@
 	}
 </style>
 
+{#if segment && segment !== 'signup'}
+	<header>
+		<nav>
+			<ul>
+				{#await fetchUser()}
+					<li>
+						<a href="#loading">my profile</a>
+					</li>
+				{:then user}
+					<li>
+						<a href={`/users/${user}`}>my profile</a>
+					</li>
+				{:catch error}
+					<li>{error}</li>
+				{/await}
+				<li>
+					<a href="/users/">users</a>
+				</li>
+				<li>
+					<button on:click={logout}>logout</button>
+				</li>
+			</ul>
+		</nav>
+	</header>
+{/if}
 <main>
 	<slot />
 </main>
