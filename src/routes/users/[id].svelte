76f4f29020be3cd4c9508d7ce4d6d70ffd8f9db2
Will Newell <newellwm@gmail.com>
2020-07-28 15:22:10 -0400

    User page gets correct user via prefetch

--- src/routes/users/[id].svelte
+++ src/routes/users/[id].svelte
@@ -1,11 +1,15 @@
-<script>
+<script context="module">
 	import apiBuilder from '../../api'
 	import StatusForm from '../../components/StatusForm.svelte'
-	const fetchData = async () => {
+	export async function preload({ params: slug }) {
 		const api = await apiBuilder()
-		const response = await api.users.getStatuses()
-		return response.data
+		const response = await api.users.getStatuses(slug)
+		return { statuses: response.data }
 	}
+</script>
+
+<script>
+	export let statuses
 	let creatingStatus = false
 	async function postStatus(status) {
 		const api = await apiBuilder()
@@ -19,26 +23,19 @@
 {:else}
 	<button on:click={() => (creatingStatus = true)}>new status</button>
 {/if}
-{#await fetchData()}
-	<p>...loading user's status log</p>
-{:then statuses}
-	<ul>
-		{#each statuses as status}
-			<li>
-				<h3>
-					Status from {new Date(status.PostedOn).toLocaleString()}
-				</h3>
-				<section>
-					<h4>{status.EventId.Type}: {status.EventId.Title}</h4>
-					<p>
-						Progression: {status.Progression}
-						<br />
-						Note: {status.Note}
-					</p>
-				</section>
-			</li>
-		{/each}
-	</ul>
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
+
+<ul>
+	{#each statuses as status}
+		<li>
+			<h3>Status from {new Date(status.PostedOn).toLocaleString()}</h3>
+			<section>
+				<h4>{status.EventId.Type}: {status.EventId.Title}</h4>
+				<p>
+					Progression: {status.Progression}
+					<br />
+					Note: {status.Note}
+				</p>
+			</section>
+		</li>
+	{/each}
+</ul>
